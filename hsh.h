54d8545a731ab101d7f0d2884096964e4bb5bf13
Stephen <nehpets0701@gmail.com>
2021-07-28 15:55:10 -0400

    initial repo setup

---
+++ hsh.h
@@ -0,0 +1,12 @@
+#ifndef HSH_H
+#define HSH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistdh>
+
+int main(int argc, char **argv);
+
+#endif
---
+++ main.c
@@ -0,0 +1,12 @@
+#include "hls.h"
+
+/*
+*main-main for shell 2
+*Return: 0 or errors
+*@argc:argc
+*@argv:argv
+*/
+int main(int argc, char **argv)
+{
+
+}
